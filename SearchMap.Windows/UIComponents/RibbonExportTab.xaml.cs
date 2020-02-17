cebecb33e89b3ecd4f6ef415b93b33ecd1472796
Louis CAUBET <louiscaubet@icloud.com>
2020-02-17 12:17:34 +0100

    Moved ribbon definition to individual components and continued filling it.

---
+++ SearchMap.Windows/UIComponents/RibbonExportTab.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SearchMap.Windows.UIComponents {
+
+    /// <summary>
+    /// Logique d'interaction pour RibbonExportTab.xaml
+    /// </summary>
+    public partial class RibbonExportTab : RibbonTabItem {
+
+        public RibbonExportTab() {
+            InitializeComponent();
+        }
+
+    }
+}
